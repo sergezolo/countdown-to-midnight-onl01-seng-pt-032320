faf4ae2beac32213aa8aaed392859c0bcb749a79
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-03-26 04:39:13 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,7 @@
-def countdown(x)
-  while x > 0
-    puts "#{x} SECOND(S)!"
-    x -= 1 
+def countdown(integer)
+  while integer > 0
+    puts "#{integer} SECOND(S)!"
+    integer -= 1 
   end
  puts "HAPPY NEW YEAR!"
 end
